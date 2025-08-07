6901c18b574cb99a25a0c64fa5e51a42fa9ccc73
kshao132 <shao.kevin@icloud.com>
2025-08-06 19:39:33 -0700

    Allow PostgreSQL db

--- chat_app/__init__.py
+++ chat_app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app.config["SECRET_KEY"] = 'secret!'
     app.config["DEBUG"] = True
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI") or "sqlite:///instance/users.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or "sqlite:///instance/users.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.register_blueprint(main)
