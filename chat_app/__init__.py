88cd7719d6aefdae1050ac851e0203e0eddcf8ab
kshao132 <shao.kevin@icloud.com>
2025-08-06 19:31:19 -0700

    Change db to allow PostgreSQL

--- chat_app/__init__.py
+++ chat_app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
+import os
 from .events import socketio
 from .routes import main
 from .extensions import db
-
+from . import models
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'secret!'
     app.config["DEBUG"] = True
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI") or "sqlite:///instance/users.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.register_blueprint(main)
